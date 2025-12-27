5845980be3291cfcce3acb197a7a2e2468c6ec39
Junior Rumiche <juniorrumichem@gmail.com>
2025-12-26 23:40:35 -0500

    feat(deployment): actualizar script de construcción para usar python genérico y mejorar la instalación de dependencias

--- build_files.sh
+++ build_files.sh
@@ -1,8 +1,10 @@
 echo "BUILD START"
-python3.9 -m pip install -r requirements.txt
+# Usar 'python' genérico que apunta al runtime de Vercel configurado
+python -m pip install --upgrade pip
+python -m pip install -r requirements.txt
 echo "Make Migration..."
-python3.9 manage.py makemigrations --noinput
-python3.9 manage.py migrate --noinput
+python manage.py makemigrations --noinput
+python manage.py migrate --noinput
 echo "Collect Static..."
-python3.9 manage.py collectstatic --noinput --clear
+python manage.py collectstatic --noinput --clear
 echo "BUILD END"
